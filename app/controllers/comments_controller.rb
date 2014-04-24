d9c98de27943ee0e5c4f9dbfd789b4d5a616fda3
vagrant <vagrant@rails-dev-box>
2014-04-24 16:24:35 +0000

    Added delete functionality to posts, topics, and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,21 @@ class CommentsController < ApplicationController
       redirect_to [@topic, @comment.post]
     end
   end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+
+    if @comment.destroy
+      flash[:notice] = "Comment removed"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Unable to remove comment. Try again"
+      redirect_to [@topic, @post]
+    end
+  end
+  
 end
