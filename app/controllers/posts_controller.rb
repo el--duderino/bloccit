524a79b09ce695ad5b451e382630414330284cdc
vagrant <vagrant@rails-dev-box>
2014-04-28 00:16:16 +0000

    Scoped topics for privacy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
 
   def show
-    @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
+    authorize @topic
+    @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments
-    authorize @post
   end
 
   def new
