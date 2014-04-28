0355dcf9a6d62b4b32ddceae0485c974138d2c90
vagrant <vagrant@rails-dev-box>
2014-04-28 18:14:30 +0000

    Cleaned up registration, email permissions, and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: "You don't have permission to do that"
   end
 
+  def after_sign_in_path_for(resource)
+    topics_path
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
