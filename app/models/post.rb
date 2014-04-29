eb3a05ec58d98c5bfd60843dfaffd46185168d17
vagrant <vagrant@rails-dev-box>
2014-04-29 00:10:21 +0000

    Created user profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   after_create :create_vote
 
   default_scope { order('rank DESC') }
+  scope :visible_to, ->(user) { user ? all : joins(:topic). where('topics.public' => true) }
 
   mount_uploader :image, PostImageUploader
 
